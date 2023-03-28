ec73f2a3dc60cbf747a4ac9ba8fe41afb1097b97
Andrea Pullen <apullenb@gmail.com>
2023-03-28 13:50:00 -0500

    add categories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import data from './data.json'
 import './App.css'
 import Games from './Games';
+import Category from './Category';
 
 function App() {
   return (
@@ -8,7 +9,12 @@ function App() {
       <div className="app-header">
          <h1>Bored</h1>
       </div>
+      <div>
         <Games games={data.games} />
+      </div>
+      <div className='categories'>
+        {data.categories.map((cat, i) => <Category data={cat} key={i} /> )}
+      </div>
     </div>
   );
 }
---
+++ src/Category.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Category(props) {
+  return (
+    <h2>{props.data.name}</h2>
+  )
+}
+
+export default Category
