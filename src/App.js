53b19e6c2b3cf0eed8a9bc283c10c007405912f8
Andrea Pullen <apullenb@gmail.com>
2023-03-28 16:09:40 -0500

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+
 import data from './data.json'
-import './App.css'
+import './App.scss';
 import Games from './Games';
 import Category from './Category';
 
 function App() {
   return (
     <div>
-      <div className="app-header">
+      <div>
          <h1>Bored</h1>
       </div>
       <div>
         <Games games={data.games} />
       </div>
-      <div className='categories'>
+      <div>
         {data.categories.map((cat, i) => <Category data={cat} key={i} /> )}
       </div>
     </div>
--- src/Games.js
+++ src/Games.js
@@ -6,7 +6,7 @@ function Games(props) {
     return (
      <div>
         <h2>{props.games[random].title} </h2>
-          <iframe src={props.games[random].src} title={props.games[random].title} width='100%'height={'500vh'} ></iframe> 
+          <iframe src={props.games[random].src} title={props.games[random].title} ></iframe> 
      </div>
   )
 }
