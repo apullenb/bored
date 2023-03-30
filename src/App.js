bf3cfca050dfe57f31b28f8ef93beb0ea0db130c
Andrea Pullen <apullenb@gmail.com>
2023-03-30 11:43:53 -0500

    add styled components

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Category from './Category';
 function App() {
   return (
     <div className='main'>
-      <div className='header'>
-         <h1>Welcome to Bored</h1>
+      <div className='header mb-5'>
+         <h1>BORED</h1>
       </div>
-      <div className=''>
+      <div className='row p-1'>
         <Games games={data.games} />
       </div>
-      <div className=''>
-        {data.categories.map((cat, i) => <div className='card shadow m-4'> <Category data={cat} key={i} /> </div> )}
+      <div className='row p-5'>
+        {data.categories.map((cat, i) => <div className='col card shadow m-3 p-5'> <Category data={cat} key={i} /> </div> )}
       </div>
     </div>
   );
--- src/Games.js
+++ src/Games.js
@@ -4,9 +4,9 @@ function Games(props) {
     const random = Math.floor(Math.random() * props.games.length)
     console.log(random)
     return (
-     <div>
+     <div className='col'>
         <h2>{props.games[random].title} </h2>
-          <iframe src={props.games[random].src} title={props.games[random].title} ></iframe> 
+          <iframe src={props.games[random].src} title={props.games[random].title} width='500px'></iframe> 
      </div>
   )
 }
