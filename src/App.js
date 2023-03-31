1edaafb33928fd710d00e712e32f2d082479465d
Andrea Pullen <apullenb@gmail.com>
2023-03-30 21:26:38 -0500

    create sections & mobile styling

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ function App() {
   //  "https://www.boredapi.com/api/",
   return (
     <div className="main">
-      <div className="header mb-5">
+      <div className="header pb-3">
         <h1>Productively Bored</h1>
         <h2>Make Your Time Well Wasted</h2>
       </div>
-      <div className="box-row">
+      <div className="box-row pb-3">
         {data.categories.map((cat, i) => (
           <Category data={cat} key={i} />
         ))}
       </div>
-      <div className="box-row">
-        <div className="box">
+      <div className="box-row pb-3">
+        <div>
+          <h4 style={{paddingLeft:'5%'}}>Try the Dumb Game of the Day!</h4>
           <Games games={data.games} />
         </div>
       </div>
--- src/Games.js
+++ src/Games.js
@@ -2,19 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Games(props) {
-    const random = Math.floor(Math.random() * props.games.length)
-    console.log(random)
-    return (
-     <Game>
-        <h3>{props.games[random].title} </h3>
-          <iframe src={props.games[random].src} title={props.games[random].title} ></iframe> 
-     </Game>
+  const random = Math.floor(Math.random() * props.games.length)
+  return (
+    <Game className='box p-4'>
+        <h3 className='pb-3'>{props.games[random].title} </h3>
+        <p>{props.games[random].description}</p>
+        <iframe src={props.games[random].src} title={props.games[random].title} height='450px'></iframe>
+    </Game>
   )
 }
 
 export default Games;
 
 const Game = styled.div`
-min-width: 65%;
+width: 35vw;
+min-width: 275px;
 
+@media screen and (max-width: 904px) {
+  width: 75vw
+}
 `
